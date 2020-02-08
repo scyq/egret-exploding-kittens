3278eb1839d54184f2f8b98712fbbdaea4369eb5
Yuancheng Zhang <en@endaye.com>
2020-02-08 15:29:37 +0800

    set ws port as 13001

--- src/config/Config.ts
+++ src/config/Config.ts
@@ -3,8 +3,8 @@ namespace Config {
     export const TimeOutMs: number = 5000;
     export const TimeWaitMs: number = 500;
     export const ResourceUrl: string = '/resource/';
-    // export const ServerUrlDebug: string = 'http://39.104.22.7:13001/';
-    export const ServerUrlDebug: string = 'http://localhost:13001/';
+    // export const ServerUrlDebug: string = 'http://39.104.22.7:13002/';
+    export const ServerUrlDebug: string = 'http://localhost:13002/';
 
     export const DebugMode: boolean = true;
     export const TestMode: boolean = true;
