5e23134b50eef36d701fdd4bec57e0813280ed97
endaye <en@endaye.com>
2020-03-17 23:41:06 +0800

    add prod ip

--- src/game/config/Config.ts
+++ src/game/config/Config.ts
@@ -5,8 +5,9 @@ namespace Config {
     export const TimeOutMs: number = 5000;
     export const TimeWaitMs: number = 500;
     export const ResourceUrl: string = '/resource/';
-    // export const ServerUrlDebug: string = 'http://39.104.22.7:13002/';
-    export const ServerUrlDebug: string = 'http://localhost:13002/';
+    export const ServerUrlDebug: string = 'http://101.200.242.57:13002/'; // prod
+    // export const ServerUrlDebug: string = 'http://39.104.22.7:13002/'; // dev
+    // export const ServerUrlDebug: string = 'http://localhost:13002/'; // local
 
     export const DebugMode: boolean = true;
     export const TestMode: boolean = true;
